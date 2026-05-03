52dd1c340c4c4a282461d68343146da8abdfaa30
Nutcha <nidnutchayodmanun@gmail.com>
2026-05-03 14:08:20 +0700

    Change from DHT22 to AHT20+BMP280

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
-#include "DHT.h"
+#include <Wire.h>
+#include <Adafruit_AHTX0.h>
+#include <Adafruit_BMP280.h>
 
 #include "config.h"
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASS;
 const char* mqtt_server = MQTT_SERVER;
 
-#define DHTPIN 4
-#define DHTTYPE DHT22
-
-DHT dht(DHTPIN, DHTTYPE);
+Adafruit_AHTX0 aht;
+Adafruit_BMP280 bmp;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
@@ -41,7 +41,18 @@ void reconnect() {
 
 void setup() {
   Serial.begin(115200);
-  dht.begin();
+  Wire.begin();
+
+  if (!aht.begin()) {
+    Serial.println("AHT20 not found!");
+    while (1) delay(10);
+  }
+
+  if (!bmp.begin(0x76)) { 
+    Serial.println("BMP280 not found!");
+    while (1) delay(10);
+  }
+
   setup_wifi();
   client.setServer(mqtt_server, 1883);
 }
@@ -52,24 +63,38 @@ void loop() {
   }
   client.loop();
 
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
+  sensors_event_t humidity_event, temp_event;
+  aht.getEvent(&humidity_event, &temp_event);
 
-  if (isnan(temperature) || isnan(humidity)) {
-    Serial.println("Failed to read from DHT");
+  float temperature = temp_event.temperature;
+  float humidity = humidity_event.relative_humidity;
+
+  float pressure = bmp.readPressure() / 100.0F;  
+  float altitude = bmp.readAltitude(1013.25);  
+
+  if (isnan(temperature) || isnan(humidity) || isnan(pressure)) {
+    Serial.println("Sensor read error!");
     return;
   }
 
-  String payload = "{\"temperature\":" + String(temperature) + ",\"humidity\":" + String(humidity) + "}";
+  String payload = "{\"temperature\":" + String(temperature, 1) +
+                   ",\"humidity\":" + String(humidity, 1) +
+                   ",\"pressure\":" + String(pressure, 1) +
+                   ",\"altitude\":" + String(altitude, 1) + "}";
+
   client.publish("weather/data", payload.c_str());
 
   Serial.print("[");
   Serial.print(millis() / 1000);
   Serial.print("s] Temp: ");
-  Serial.print(temperature);
-  Serial.print(" °C | Humidity: ");
-  Serial.print(humidity);
-  Serial.println(" %");
+  Serial.print(temperature, 1);
+  Serial.print("°C | Humidity: ");
+  Serial.print(humidity, 1);
+  Serial.print("% | Pressure: ");
+  Serial.print(pressure, 1);
+  Serial.print("hPa | Altitude: ");
+  Serial.print(altitude, 1);
+  Serial.println("m");
 
   delay(5000);
 }
