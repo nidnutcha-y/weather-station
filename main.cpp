3e28c63420e8dd5ea3e8d621c4dbe610fd162655
Nutcha <nidnutchayodmanun@gmail.com>
2026-05-03 14:57:02 +0700

    Little update

--- main.cpp
+++ main.cpp
@@ -1,88 +1,50 @@
-#include <WiFi.h>
-#include <PubSubClient.h>
 #include <Wire.h>
-#include <Adafruit_AHTX0.h>
 #include <Adafruit_BMP280.h>
+#include <Adafruit_AHTX0.h>
+#include <WiFi.h>
 
 #include "config.h"
-const char* ssid = WIFI_SSID;
-const char* password = WIFI_PASS;
-const char* mqtt_server = MQTT_SERVER;
+const char* ssid = "Your wifi name";
+const char* password = "Your wifi password";
 
 Adafruit_AHTX0 aht;
 Adafruit_BMP280 bmp;
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-void setup_wifi() {
-  delay(10);
-  Serial.println("Connecting to WiFi...");
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nWiFi connected");
-}
-
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT...");
-    if (client.connect("ESP32_Client")) {
-      Serial.println("connected");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" retrying...");
-      delay(2000);
-    }
-  }
-}
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 
   if (!aht.begin()) {
     Serial.println("AHT20 not found!");
     while (1) delay(10);
   }
 
-  if (!bmp.begin(0x76)) { 
-    Serial.println("BMP280 not found!");
-    while (1) delay(10);
+  if (!bmp.begin(0x76)) {
+    if (!bmp.begin(0x77)) {
+      Serial.println("BMP280 not found!");
+      while (1) delay(10);
+    }
   }
-
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-
   sensors_event_t humidity_event, temp_event;
   aht.getEvent(&humidity_event, &temp_event);
 
   float temperature = temp_event.temperature;
   float humidity = humidity_event.relative_humidity;
-
-  float pressure = bmp.readPressure() / 100.0F;  
-  float altitude = bmp.readAltitude(1013.25);  
-
-  if (isnan(temperature) || isnan(humidity) || isnan(pressure)) {
-    Serial.println("Sensor read error!");
-    return;
-  }
-
-  String payload = "{\"temperature\":" + String(temperature, 1) +
-                   ",\"humidity\":" + String(humidity, 1) +
-                   ",\"pressure\":" + String(pressure, 1) +
-                   ",\"altitude\":" + String(altitude, 1) + "}";
-
-  client.publish("weather/data", payload.c_str());
+  float pressure = bmp.readPressure() / 100.0F;
+  float altitude = bmp.readAltitude(1013.25);
 
   Serial.print("[");
   Serial.print(millis() / 1000);
